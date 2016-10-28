095078495e740e0c208c7546a3d8b077012eb49f
dwitherell <devonwitherell1@gmail.com>
2016-10-28 00:43:48 -0600

    Fix seekbar attribute grabbing
    * Also remove from attrs as we have always used android: for max
    
    Change-Id: Idc1551e1e79c106a9418a491f7bef51c2e82635f

--- src/com/android/settings/arrow/preferences/CustomSeekBarPreference.java
+++ src/com/android/settings/arrow/preferences/CustomSeekBarPreference.java
@@ -51,7 +51,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
         final TypedArray a = context.obtainStyledAttributes(
                 attrs, R.styleable.CustomSeekBarPreference);
 
-        mMax = attrs.getAttributeIntValue(SETTINGS_NS, "max", 100);
+        mMax = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
         mMin = attrs.getAttributeIntValue(SETTINGS_NS, "min", 0);
         mDefaultValue = attrs.getAttributeIntValue(ANDROIDNS, "defaultValue", -1);
         if (mDefaultValue > mMax) {
