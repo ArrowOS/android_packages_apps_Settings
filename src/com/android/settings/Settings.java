778e7408b026c1f79f6833f8d18b60d466a3c831
maxwen <max.weninger@gmail.com>
2017-08-28 17:15:28 +0200

    Global Actions with Advanced Reboot [2/2]
    Change-Id: I601b4fb4a25e528767bcad7861446c2e8cdd377d
    [jaysonedson@gmail.com: Change setting for lockdown]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -240,4 +240,8 @@ public class Settings extends SettingsActivity {
      */
     public static class MediaControlsSettingsActivity extends SettingsActivity {}
 
+    /**
+     * Activity for Customization Extras
+     */
+    public static class GlobalActionsSettingsActivity extends SettingsActivity { /* empty */ }
 }
