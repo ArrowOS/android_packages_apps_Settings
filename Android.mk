b758c6f2f762a80cc122bafb91e0a6daccd5f0b2
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate ButtonSettings into System Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    @mydongistiny: fixed up some for dashboard
    
    arrow edits: import button settings icon
    
    Buttons: Update drawer icon 5ea7868c5f618f240ca7043179266c5b862daa1b
    
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4

--- Android.mk
+++ Android.mk
@@ -40,7 +40,21 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     jsr305 \
     settings-logtags
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res \
+    packages/apps/CustomSettings/res
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.preference \
+    --extra-packages android.support.v14.preference \
+    --extra-packages android.support.v17.preference \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.v7.recyclerview
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
