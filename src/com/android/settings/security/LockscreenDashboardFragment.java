7dbbed0bb69f861ecd3063125e5396b4d24c5917
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-09-28 22:11:35 +0000

    inline fingerprint unlock keystore switch into lockscreen fragment [2/2]
    Converted to aosp PreferenceController in order to be inlined
    within Lockscreen Preferences fragment. Preference will be set
    to hidden using the method isAvailable() on the bool state of
    our custom config.
    
    reference:
    https://github.com/AOSPA/android_packages_apps_Settings/commit/047a26b496df3bb68f39b27852e39b73a5fb76a1
    
    android 10.0 arrow-edits:- migrate to androidx, remove the search indexing as its been
     already handled in settingsintelligence
    
    Change-Id: I46af19970570096aca88d62297aa36ecfbfc0653

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -115,7 +115,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
                 new OwnerInfoPreferenceController(context, this, lifecycle);
         controllers.add(mOwnerInfoPreferenceController);
 	controllers.add(new FPVibrationPreferenceController(context));
-
+	controllers.add(new FPUnlockKeystorePreferenceController(context));
         return controllers;
     }
 
@@ -151,6 +151,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
                     controllers.add(new OwnerInfoPreferenceController(
                             context, null /* fragment */, null /* lifecycle */));
 		    controllers.add(new FPVibrationPreferenceController(context));
+		    controllers.add(new FPUnlockKeystorePreferenceController(context));
                     return controllers;
                 }
 
