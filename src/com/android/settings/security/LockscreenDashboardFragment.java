cc63f517df19d02b6d4a61f1978c4f084e2c2e6e
Alex Cruz <alex@dirtyunicorns.com>
2018-10-10 01:13:40 +0200

    lockdown on power menu [2/2]
    Arrow edits
    * configurable lockscreen visibility
    * remove the lockdown from security
    * moved to advanced power menu
    
    based on @Mazda--'s advanced power menu commits
    
    Change-Id: I1792408c5a6503f4dab19bdf883934961d02d0ed

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -91,7 +91,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         mOwnerInfoPreferenceController =
                 new OwnerInfoPreferenceController(context, this, lifecycle);
         controllers.add(mOwnerInfoPreferenceController);
-        controllers.add(new LockdownButtonPreferenceController(context));
+        //controllers.add(new LockdownButtonPreferenceController(context));
 
         return controllers;
     }
@@ -122,7 +122,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
                             KEY_ADD_USER_FROM_LOCK_SCREEN, null /* lifecycle */));
                     controllers.add(new OwnerInfoPreferenceController(
                             context, null /* fragment */, null /* lifecycle */));
-                    controllers.add(new LockdownButtonPreferenceController(context));
+                    //controllers.add(new LockdownButtonPreferenceController(context));
                     return controllers;
                 }
 
