27714422a53c6a1d38bb819200fae2ea2054b238
Kshitij Gupta <kshitijgm@gmail.com>
2019-01-04 04:01:57 +0530

    notch-city: Add 3 mode display cutout handler [3/3]
    - Add the respective control in here
    
    arrow-edits:
    - Migrate to androidx
    - Add our cutout preference controller
    
    Change-Id: I38fb439046db010b8b45f2bf9b83b7ce8879e044
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

---
+++ src/com/android/settings/display/HideNotchPreferenceController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018 ArrowOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import com.android.settings.DisplaySettings;
+import androidx.preference.SwitchPreference;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.util.List;
+
+public class HideNotchPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_HIDE_NOTCH = "cutout_settings";
+
+    public HideNotchPreferenceController(Context context) {
+	super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+	return KEY_HIDE_NOTCH;
+    }
+
+   @Override
+    public boolean isAvailable() {
+	return !mContext.getResources().getString(
+                com.android.internal.R.string.config_mainBuiltInDisplayCutout).isEmpty();
+    }
+
+    @Override
+    public void updateNonIndexableKeys(List<String> keys) {
+        keys.add(getPreferenceKey());
+    }
+}
