15793134f4b4dfed149825a1bd1c3607fd386f3f
Kshitij Gupta <kshitijgm@gmail.com>
2019-01-04 04:01:57 +0530

    notch-city: Add 3 mode display cutout handler [3/3]
    - Add the respective control in here
    
    arrow-edits:
    - Migrate to androidx
    - Add our cutout preference controller
    - Add Search Keywords
    
    Change-Id: I38fb439046db010b8b45f2bf9b83b7ce8879e044
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -24,6 +24,7 @@ import android.provider.SearchIndexableResource;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
+import com.android.settings.display.HideNotchPreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
@@ -83,6 +84,7 @@ public class DisplaySettings extends DashboardFragment {
             Context context, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new CameraGesturePreferenceController(context));
+        controllers.add(new HideNotchPreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new NightDisplayPreferenceController(context));
         controllers.add(new NightModePreferenceController(context));
