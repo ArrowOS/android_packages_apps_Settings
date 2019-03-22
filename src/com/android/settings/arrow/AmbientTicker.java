PotatoProject/packages_apps_Fries

Ambient display music ticker [2/3]
Change-Id: Ieb1af0e403322177aebf3a4cea4c4ec574d8c740

src/com/potato/fries/fragments/AmbientTicker.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2017 The ABC rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.potato.fries.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AmbientTicker extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ambient_ticker);
+
+        getActivity().getActionBar().setTitle(R.string.force_ambient_for_media_pref_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.ambient_ticker_footer);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.FRIES;
+    }
+}

ArrowOS/android_packages_apps_Settings

src/com/android/settings/arrow/AmbientTicker.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2017 The ABC rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.arrow;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AmbientTicker extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ambient_ticker);
+
+        getActivity().getActionBar().setTitle(R.string.force_ambient_for_media_pref_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.ambient_ticker_footer);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ARROW;
+    }
+}