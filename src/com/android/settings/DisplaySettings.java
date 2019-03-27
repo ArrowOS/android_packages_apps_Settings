c3448e7296c53d76ebd2b1e44b4042bcb86b1d77
Bauuuuu <abansode@outlook.com>
2019-03-27 10:55:26 +0100

    Revert "Add Font preference for font manager"
    This reverts commit cf0e491d9e3030678128d3a739abbba82ac71852.
    
    Change-Id: I3f41463b9d9369a9d43e58426f0ce0b2acfbab08

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -42,7 +42,6 @@ import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.WallpaperPreferenceController;
 import com.android.settings.display.HideNotchPreferenceController;
-import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -98,7 +97,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new NightModePreferenceController(context));
         controllers.add(new ProximityOnWakePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
-        controllers.add(new FontPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new AmbientDisplayPreferenceController(
                 context,
                 new AmbientDisplayConfiguration(context),
