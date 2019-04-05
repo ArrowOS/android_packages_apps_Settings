e5124ce9ea39c75f615545b13875d6599c20411d
Venkata Jagadeesh Garaga <vgaraga@codeaurora.org>
2019-04-05 17:38:18 +0530

    BT: Don't display previously connected devices in BLE_ON
    When user turn off BT from settings menu with BLE APPs
    running, cached devices will not be cleared until bt off.
    
    Previously connected devices list will be displayed
    in BT settings screen
    
    Hence disable previously connected list in BLE_ON state.
    
    CRs-Fixed: 2693837
    Change-Id: I11df81819d0be0d3f7d7598b53057afcb5245901

--- src/com/android/settings/connecteddevice/PreviouslyConnectedDevicePreferenceController.java
+++ src/com/android/settings/connecteddevice/PreviouslyConnectedDevicePreferenceController.java
@@ -28,7 +28,13 @@ import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
+import android.bluetooth.BluetoothAdapter;
 
+import com.android.settingslib.bluetooth.BluetoothCallback;
+import com.android.settingslib.bluetooth.LocalBluetoothAdapter;
+import com.android.settingslib.bluetooth.LocalBluetoothManager;
+import com.android.settingslib.bluetooth.CachedBluetoothDevice;
+import com.android.settings.bluetooth.Utils;
 import com.android.settings.R;
 import com.android.settings.bluetooth.BluetoothDevicePreference;
 import com.android.settings.bluetooth.BluetoothDeviceUpdater;
@@ -45,7 +51,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PreviouslyConnectedDevicePreferenceController extends BasePreferenceController
-        implements LifecycleObserver, OnStart, OnStop, DevicePreferenceCallback {
+        implements LifecycleObserver, OnStart, OnStop, DevicePreferenceCallback, BluetoothCallback {
 
     private static final String TAG = "PreviouslyDevicePreController";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
@@ -58,6 +64,8 @@ public class PreviouslyConnectedDevicePreferenceController extends BasePreferenc
     private final List<Preference> mDockDevicesList = new ArrayList<>();
 
     private PreferenceGroup mPreferenceGroup;
+    private LocalBluetoothAdapter mLocalAdapter;
+    private LocalBluetoothManager manager;
     private BluetoothDeviceUpdater mBluetoothDeviceUpdater;
     private DockUpdater mSavedDockUpdater;
     private BluetoothAdapter mBluetoothAdapter;
@@ -82,6 +90,10 @@ public class PreviouslyConnectedDevicePreferenceController extends BasePreferenc
                 context).getDockUpdaterFeatureProvider().getSavedDockUpdater(context, this);
         mIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        manager = Utils.getLocalBtManager(context);
+         if ( manager != null) {
+           mLocalAdapter = manager.getBluetoothAdapter();
+         }
     }
 
     @Override
@@ -111,6 +123,7 @@ public class PreviouslyConnectedDevicePreferenceController extends BasePreferenc
         mBluetoothDeviceUpdater.registerCallback();
         mSavedDockUpdater.registerCallback();
         mContext.registerReceiver(mReceiver, mIntentFilter);
+        manager.getEventManager().registerCallback(this);
     }
 
     @Override
@@ -118,6 +131,7 @@ public class PreviouslyConnectedDevicePreferenceController extends BasePreferenc
         mBluetoothDeviceUpdater.unregisterCallback();
         mSavedDockUpdater.unregisterCallback();
         mContext.unregisterReceiver(mReceiver);
+        manager.getEventManager().unregisterCallback(this);
     }
 
     public void init(DashboardFragment fragment) {
@@ -198,6 +212,46 @@ public class PreviouslyConnectedDevicePreferenceController extends BasePreferenc
 
         addPreference();
         updatePreferenceVisibility();
+    }
+
+    @Override
+    public void onBluetoothStateChanged(int bluetoothState) {
+        // do nothing
+    }
+
+    @Override
+    public void onScanningStateChanged(boolean started) {
+        // do nothing
+    }
+
+    @Override
+    public void onDeviceAdded(CachedBluetoothDevice cachedDevice) {
+       // do nothing
+    }
+
+    @Override
+    public void onDeviceDeleted(CachedBluetoothDevice cachedDevice) {
+        // do nothing
+    }
+
+    @Override
+    public void onDeviceBondStateChanged(CachedBluetoothDevice cachedDevice, int bondState) {
+        // do nothing
+    }
+
+    @Override
+    public void onConnectionStateChanged(CachedBluetoothDevice cachedDevice, int state) {
+        // do nothing
+    }
+
+    @Override
+    public void onActiveDeviceChanged(CachedBluetoothDevice activeDevice, int bluetoothProfile) {
+        // do nothing
+    }
+
+    @Override
+    public void onAudioModeChanged() {
+       // do nothing
     }
 
     @VisibleForTesting
