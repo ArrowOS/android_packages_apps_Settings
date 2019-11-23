db4d6f5ccf7df2dbcfb732a70b87e1c1d603509c
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-11-23 17:38:03 +0100

    Themes: Get rid of the accents,icons and font preferences
    - We now ship ThemePicker.
    - Dark mode, ColorBucket stay here for now and later will be migrated into
      the ThemePicker.
    
    Change-Id: I0fb3665918a3dd7cca013b60d57ea0c1b2db4f11

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -41,8 +41,6 @@ import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
-import com.android.settings.development.OverlayCategoryPreferenceController;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -97,14 +95,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
-	controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.accent_color"));
 	controllers.add(new CustomOverlayPreferenceController(context,
 		"android.theme.customization.custom_overlays"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.adaptive_icon_shape"));
         return controllers;
     }
 
