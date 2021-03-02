570739a971bde57115aae4f2311e4bda979ee010
kubersharma001 <kubersharma001@gmail.com>
2021-03-02 15:34:02 +0000

    Settings: fixup! android-11.0.0_r33 merge
     * Respect the changes done in: "add back battery level animation"
    
    Change-Id: I49726fb8ea9c837ffc4b74e8af753ba3ff331003

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -577,9 +577,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     protected void restartBatteryStatsLoader(@BatteryUpdateType int refreshType) {
         super.restartBatteryStatsLoader(refreshType);
         // Update battery header if battery is present.
-        if (mIsBatteryPresent) {
+       /* if (mIsBatteryPresent) {
             mBatteryHeaderPreferenceController.quickUpdateHeaderPreference();
-        }
+        } */
     }
 
     @Override
